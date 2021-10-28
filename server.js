2da401b0b176d0059424cec775e65f601d9b20d7
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-28 17:41:00 -0400

    worked server.js

--- server.js
+++ server.js
@@ -6,3 +6,10 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+app.use(express.static("client/build"));
+}
