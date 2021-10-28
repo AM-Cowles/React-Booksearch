8faffc6fa155843be75c0b3c75cfd246fc96952b
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-28 17:42:07 -0400

    worked server.js

--- server.js
+++ server.js
@@ -13,3 +13,13 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
 app.use(express.static("client/build"));
 }
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/reactBooks", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useFindAndModify: false
+    }
+);
