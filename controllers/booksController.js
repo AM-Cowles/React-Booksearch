9dff6c48fbc279fc05ac27e10c4012489da0a8bb
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-28 18:27:26 -0400

    worked booksController.js

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -21,3 +21,9 @@ create: function(req, res) {
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err));
 },
+update: function(req, res) {
+    db.Book
+    .findOneAndUpdate({ _id: req.params.id }, req.body)
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
+},
